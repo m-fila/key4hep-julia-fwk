8bd9c550f8afa0fddd6f8f5980af21706d621a4f
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2025-05-09 15:36:05 +0200

    add compilation time plot

--- benchmark/throughput_vis.jl
+++ benchmark/throughput_vis.jl
@@ -3,6 +3,7 @@ using CSV
 using DataFrames
 using ArgParse
 using Statistics
+using Printf
 
 function parse_args(args)
     s = ArgParseSettings(description = "Visualize the throughput from timing CSV files")
@@ -25,8 +26,12 @@ function parse_args(args)
         help = "Do not include comparison with linear scaling for throughput"
         action = :store_true
 
-        "--no-gctime"
+        "--no-gc-time"
         help = "Do not include GC time plot"
+        action = :store_true
+
+        "--no-compile-time"
+        help = "Do not include compile time plot"
         action = :store_true
 
         "--median"
@@ -43,12 +48,19 @@ function (@main)(args)
     @info "Using metric: $(metric)"
 
     df = vcat(map(file -> DataFrame(CSV.File(file)), parsed_args["input"])...)
+    println("DataFrame: ", df)
     cols = [:time, :throughput]
 
-    if !parsed_args["no-gctime"]
-        df.gctime_percent = 100 * df.gctime ./ df.time
-        push!(cols, :gctime_percent)
+    if !parsed_args["no-gc-time"]
+        df.gctime_fraction = df.gctime ./ df.time
+        push!(cols, :gctime_fraction)
     end
+    if !parsed_args["no-compile-time"]
+        df.compile_time_fraction = df.compile_time ./ df.time
+        push!(cols, :compile_time_fraction)
+        println("Compile time fraction: ", df.compile_time_fraction)
+    end
+
     gdf = groupby(df, [:threads, :event_count, :max_concurrent])
     df = combine(gdf, cols .=> metric .=> (col -> Symbol(col, "_metric")))
     df = sort(df, [:threads, :event_count, :max_concurrent])
@@ -81,15 +93,28 @@ function (@main)(args)
     plot(df.threads, df.throughput_metric ./ df.perfect_scaling, label = "Ratio",
          title = "Throughput per thread scaling", xlabel = "Number of threads",
          ylabel = "Throughput per thread ratio", marker = (:circle, 5), linewidth = 3,
+         yformatter = x -> Printf.@sprintf("%.1f%%", 100*x),
          xguidefonthalign = :right, yguidefontvalign = :top)
     savefig(output_file)
     @info "Saved plot to $output_file"
 
-    if !parsed_args["no-gctime"]
+    if !parsed_args["no-gc-time"]
         output_file = "gc.png"
-        plot(df.threads, df.gctime_percent_metric, label = "GC time",
+        plot(df.threads, df.gctime_fraction_metric, label = "GC time",
              title = "Garbage collection time", xlabel = "Number of threads",
-             ylabel = "GC time (%)", marker = (:circle, 5), linewidth = 3,
+             ylabel = "GC time", marker = (:circle, 5), linewidth = 3,
+             yformatter = x -> Printf.@sprintf("%.1f%%", 100*x),
+             xguidefonthalign = :right, yguidefontvalign = :top)
+        savefig(output_file)
+        @info "Saved plot to $output_file"
+    end
+
+    if !parsed_args["no-compile-time"]
+        output_file = "compile.png"
+        plot(df.threads, df.compile_time_fraction_metric, label = "compile time",
+             title = "Compilation time", xlabel = "Number of threads",
+             ylabel = "Compile time", marker = (:circle, 5), linewidth = 3,
+             yformatter = x -> Printf.@sprintf("%.1f%%", 100*x),
              xguidefonthalign = :right, yguidefontvalign = :top)
         savefig(output_file)
         @info "Saved plot to $output_file"
